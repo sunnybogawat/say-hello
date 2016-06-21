8c95f73a661bcc64f35c5196d2c0b86d46007831
sunny <sunny.bogawat@sungardas.com>
2016-06-21 09:01:03 +0530

    First commit

---
+++ src/hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, World one zero seven</h1>
+  }
+}
+export default Hello;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ["./src/main.js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "hello.js",
+    library: 'hello',
+    libraryTarget: 'umd',
+    externals: {
+        react: 'react',
+    }
+  },
+};
